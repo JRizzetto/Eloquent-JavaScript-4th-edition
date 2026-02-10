c358db0293b8cdd3824ea277247058d75a72a7c4
JRizzetto <jeffersonrizzetto@gmail.com>
2026-02-10 11:19:34 -0300

    Chapter 1 - Values, Types, and Operators

---
+++ chapter-1/index.js
@@ -0,0 +1,25 @@
+console.log('A newline character is written like "\\n".');
+console.log("con" + "cat" + "e" + "nate");
+console.log(`half of 100 is ${100 / 2}`);
+
+console.log(true ? 1 : 0);
+console.log(false ? 1 : 0);
+
+console.log(8 * null);
+console.log("5" - 1);
+console.log("5" + 1);
+console.log("five" * 2);
+console.log(false == 0);
+console.log(null == undefined);
+console.log(null == 0);
+console.log(null || "user");
+// → user
+console.log("Agnes" || "user");
+// → Agnes
+
+console.log(0 || 100);
+// → 100
+console.log(0 ?? 100);
+// → 0
+console.log(null ?? 100);
+// → 100
