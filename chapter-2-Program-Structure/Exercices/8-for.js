28e19561ddbba53d0b490a417e33f20d97cac160
JRizzetto <jeffersonrizzetto@gmail.com>
2026-02-17 22:19:46 -0300

    chapter-2-Program-Structure = Exercícios looping for

--- chapter-2-Program-Structure/Exercices/8-for.js
+++ chapter-2-Program-Structure/Exercices/8-for.js
@@ -8,14 +8,75 @@ Os números naturais são:
 1 2 3 4 5 6 7 8 9 10
 */
 
-let result = 0;
-let nTermos = 7;
+// let count = "";
 
-for (let i = 1; i <= nTermos; i++) {
-  result += i;
+// for (let i = 1; i <= 10; i++) {
+//   count += " " + i;
+// }
+// console.log("The natural numbers are: " + count);
 
-  console.log("Digite o número de termos: " + nTermos);
-  console.log("Os números naturais são: " + i);
-}
+/*
+2. Sum of First 10 Natural Numbers
+Write a program in C++ to find the sum of the first 10 natural numbers.
 
-console.log(result);
+Sample Output:
+Find the first 10 natural numbers:
+---------------------------------------
+The natural numbers are:
+1 2 3 4 5 6 7 8 9 10
+The sum of first 10 natural numbers: 55
+*/
+
+// let count = "";
+// let sum = 0;
+
+// for (let i = 1; i <= 10; i++) {
+//   count += " " + i;
+//   sum += i;
+// }
+// console.log("The natural numbers are: " + count);
+// console.log("The sum of first 10 natural numbers: " + sum);
+
+/*
+3. Display n Terms of Natural Numbers and Their Sum
+
+Write a program in C++ to display n terms of natural numbers and their sum.
+
+Sample Output:
+Input a number of terms: 7
+The natural numbers upto 7th terms are:
+1 2 3 4 5 6 7
+The sum of the natural numbers is: 28
+*/
+
+// let input = 7;
+// let naturalNumbers = "";
+// let count = 0;
+
+// for (let i = 1; i <= input; i++) {
+//   naturalNumbers += " " + i;
+//   count += i;
+// }
+
+// console.log(`Input a number of terms: ${input}`);
+// console.log(`The natural numbers upto ${input}th terms are: ${naturalNumbers}`);
+// console.log(`The sum of the natural numbers is: ${count}`);
+
+// 4. Find Perfect Numbers Between 1 and 500
+
+// let result = 0;
+// let nTermos = 500;
+
+// for (let i = 1; i <= nTermos; i++) {
+//   result = 0;
+
+//   for (x = 1; x < i; x++) {
+//     if (i % x === 0) {
+//       result += x;
+//     }
+//   }
+
+//   if (result === i) {
+//     console.log(i);
+//   }
+// }
