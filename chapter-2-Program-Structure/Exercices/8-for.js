c8416d232be75746b21a442e4ff2723bf999bec2
JRizzetto <jeffersonrizzetto@gmail.com>
2026-02-18 12:58:49 -0300

    chapter-2-Program-Structure = Exercícios 5, 6, 7, 8

--- chapter-2-Program-Structure/Exercices/8-for.js
+++ chapter-2-Program-Structure/Exercices/8-for.js
@@ -80,3 +80,115 @@ The sum of the natural numbers is: 28
 //     console.log(i);
 //   }
 // }
+
+/*
+5. Check Whether a Number is Prime or Not
+
+Write a program in C++ to check whether a number is prime or not.
+
+Sample Output:
+Input a number to check prime or not: 13
+The entered number is a prime number.
+*/
+
+// let inputNumber = 6;
+// let isPrime = true;
+
+// for (let i = 2; i < inputNumber; i++) {
+//   if (inputNumber % i === 0) {
+//     keyControl = false;
+//     break;
+//   }
+// }
+
+// if (keyControl) {
+//   console.log("The entered number is a prime number.");
+// } else {
+//   console.log("is NOT prime");
+// }
+
+/*
+6. Find Prime Numbers Within a Range
+
+Write a program in C++ to find a prime number within a range.
+
+Input number for starting range: 1
+Input number for ending range: 100
+The prime numbers between 1 and 100 are:
+2 3 5 7 11 13 17 19 23 29 31 37 41 43 47 53 59 61 67 71 73 79 83 89 97
+The total number of prime numbers between 1 to 100 is: 25
+*/
+
+// let result = [];
+// let rangeInput = 37;
+// let isPrime = true;
+
+// for (let i = 2; i <= rangeInput; i++) {
+//   isPrime = true;
+
+//   for (let x = 2; x * x <= i; x++) {
+//     if (i % x === 0) {
+//       isPrime = false;
+//       break;
+//     }
+//   }
+
+//   if (isPrime) {
+//     result.push(i);
+//   }
+// }
+
+// console.log(result);
+
+/*
+7. Find the Factorial of a Number
+Write a program in C++ to find the factorial of a number.
+
+Sample output:
+Input a number to find the factorial: 5
+The factorial of the given number is: 120
+*/
+
+// let input = 5;
+// let factorial = 1;
+
+// for (let i = 1; i <= input; i++) {
+//   factorial = factorial * i;
+// }
+
+// console.log(`Input a number to find the factorial: ${input}`);
+// console.log(`The factorial of the given number is: ${factorial}`);
+
+/*
+8. Find the Last Prime Number Before a Given Number
+Write a program in C++ to find the last prime number that occurs before the entered number.
+Sample Output:
+
+Input a number to find the last prime number occurs before the number: 50
+47 is the last prime number before 50
+*/
+
+// let inputNumber = 50;
+// let primeArray = [];
+// let isPrime = true;
+
+// for (let i = 2; i < inputNumber; i++) {
+//   isPrime = true;
+
+//   for (let x = 2; x * x <= i; x++) {
+//     if (i % x === 0) {
+//       isPrime = false;
+//       break;
+//     }
+//   }
+
+//   if (isPrime) primeArray.push(i);
+// }
+
+// console.log(
+//   `Input a number to find the last prime number occurs before the number: ${inputNumber}`,
+// );
+
+// console.log(
+//   `${primeArray[primeArray.length - 1]} is the last prime number before ${inputNumber}`,
+// );
